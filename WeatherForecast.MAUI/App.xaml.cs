28deac3db0ccb32eb6c2add5e0f079b44bcf1a84
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-09 17:08:44 -0300

    feat: initial commit

---
+++ WeatherForecast.MAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherForecast.MAUI;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
