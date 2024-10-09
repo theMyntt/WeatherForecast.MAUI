c435465e3ab46063e2e0d57401076b339d92a2b2
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-09 17:21:37 -0300

    refactor: remove trash code

---
+++ WeatherForecast.MAUI/Views/FindLocations.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecast.MAUI.Views;
+
+public partial class FindLocations : ContentPage
+{
+    public FindLocations()
+    {
+        InitializeComponent();
+    }
+}
