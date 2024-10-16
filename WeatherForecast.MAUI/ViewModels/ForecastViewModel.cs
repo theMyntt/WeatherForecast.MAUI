f15f8dc060b1bf76b8a5e9cae5b835e9161e654b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-16 18:01:23 -0300

    fix: calling get location forecast two times

--- WeatherForecast.MAUI/ViewModels/ForecastViewModel.cs
+++ WeatherForecast.MAUI/ViewModels/ForecastViewModel.cs
@@ -11,11 +11,6 @@ public partial class ForecastViewModel : ObservableObject
 
     private readonly ForecastService _service = new();
 
-    public ForecastViewModel()
-    {
-        _ = GetLocationForecast();
-    }
-
     public async Task GetLocationForecast()
     {
         Location = await _service.Perform(CityCode ?? 244);
